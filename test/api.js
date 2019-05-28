eb66bf04eb65e080b4237a66e78faa6651bc1b03
fisker <lionkay@gmail.com>
2019-05-28 14:11:36 +0800

    test: remove useless test

--- test/api.js
+++ test/api.js
@@ -2,20 +2,6 @@ import test from 'ava'
 
 import {parse, load, stringify} from '../src'
 
-test('stringify options(String)', t => {
-  t.is(
-    stringify({left: 'phalange'}, 'toml'),
-    stringify({left: 'phalange'}, {type: 'toml'})
-  )
-})
-
-test('stringify options(Boolean)', t => {
-  t.is(
-    stringify({left: 'phalange'}, true),
-    stringify({left: 'phalange'}, {pretty: true})
-  )
-})
-
 test('parse(data)', t => {
   t.deepEqual(
     parse('left = phalange'),
