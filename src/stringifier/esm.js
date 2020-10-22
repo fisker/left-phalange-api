18e77e72ccac46c6d16a618a54a0bf7f1e23230a
fisker <lionkay@gmail.com>
2020-10-22 15:49:23 +0800

    fix: esm stringify

--- src/stringifier/esm.js
+++ src/stringifier/esm.js
@@ -1,7 +1,7 @@
 import json5Stringify from './json5'
 
 function stringifyEsm(data, options) {
-  return `export default  ${json5Stringify(data, options)}`
+  return `export default ${json5Stringify(data, options)}`
 }
 
 export default stringifyEsm
